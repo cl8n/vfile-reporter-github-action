d725ea5de3c14b500f2bf19aab16bbe1d9924d60
clayton <me@clayton.cc>
2023-09-28 00:03:51 -0700

    Format

--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,37 @@ import type { VFileMessage } from 'vfile-message';
 
 export default reporter;
 export function reporter(files: VFile | VFile[]): string {
-  if (!Array.isArray(files)) {
-    files = [files];
-  }
+	if (!Array.isArray(files)) {
+		files = [files];
+	}
 
-  return files.map(reportFile).join('');
+	return files.map(reportFile).join('');
 }
 
 function reportFile(file: VFile): string {
-  return file.messages.map(reportMessage).join('');
+	return file.messages.map(reportMessage).join('');
 }
 
 function reportMessage(message: VFileMessage): string {
-  const startLine = message.position?.start.line ?? message.line;
-  const startColumn = message.position?.start.column ?? message.column;
-  const end = message.position?.end;
+	const startLine = message.position?.start.line ?? message.line;
+	const startColumn = message.position?.start.column ?? message.column;
+	const end = message.position?.end;
 
-  const annotationParts: string[] = [];
-  if (message.source != null && message.ruleId != null)
-    annotationParts.push(`title=${message.source}:${message.ruleId}`);
-  if (message.file != null)
-    annotationParts.push(`file=${message.file}`);
-  if (startLine != null)
-    annotationParts.push(`line=${startLine}`);
-  if (startColumn != null)
-    annotationParts.push(`col=${startColumn}`);
-  if (end != null)
-    annotationParts.push(`endLine=${end.line}`, `endColumn=${end.column}`);
+	const annotationParts: string[] = [];
+	if (message.source != null && message.ruleId != null)
+		annotationParts.push(`title=${message.source}:${message.ruleId}`);
+	if (message.file != null)
+		annotationParts.push(`file=${message.file}`);
+	if (startLine != null)
+		annotationParts.push(`line=${startLine}`);
+	if (startColumn != null)
+		annotationParts.push(`col=${startColumn}`);
+	if (end != null)
+		annotationParts.push(`endLine=${end.line}`, `endColumn=${end.column}`);
 
-  let annotationBody = message.reason;
-  if (message.url != null)
-    annotationBody += ` — ${message.url}`;
+	let annotationBody = message.reason;
+	if (message.url != null)
+		annotationBody += ` — ${message.url}`;
 
-  return `::${message.fatal ? 'error' : 'warning'} ${annotationParts.join(',')}::${annotationBody}\n`;
+	return `::${message.fatal ? 'error' : 'warning'} ${annotationParts.join(',')}::${annotationBody}\n`;
 }
